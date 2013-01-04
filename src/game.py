2c648b33e79a96b5099aca6a2ce6fe4184e2b2f0
Chris Atthestudy <chris@the-study.net>
2013-01-04 13:56:56 +0000

    First shot (ho, ho!) at weapon fire

--- src/game.py
+++ src/game.py
@@ -9,12 +9,11 @@ import random
 import pygame
 from pygame.locals import *
 
-# ==============================================================================
-
 class ParallaxScroller():
     """
     Implements a vertically scrolling area of the screen, wrapping around at the 
-    top and bottom edges.
+    top and bottom edges. For this game this is used for the star-fields in the
+    background.
     """
     def __init__(self, imagename, x, y, speed):
         self.image = pygame.image.load(imagename).convert_alpha()
@@ -49,36 +48,38 @@ class Animation():
     on_cycle = None
     rect = None
     
-    def __init__(self, imagename, frame_width, speed, on_cycle = None):
+    def __init__(self, image, frame_width, speed, on_cycle = None):
         """
         Initialises the sprite, loading the base image and extracting the
         frames.
     
         Params:
-            imagename : filename of the sprite image
+            image : filename of the sprite image OR actual image
             frame_width : width of each individual frame
             speed : frames per second (max. of 1000)
             on_cycle: optional callback to be invoked at the end of a cycle
         """
-        # Load the image, using convert_alpha() to ensure that any transparency
-        # is preserved.
-        if self.sprite_image == None:
-            self.sprite_image = pygame.image.load(imagename).convert_alpha()
+        if isinstance(image, basestring):
+            # Load the image, using convert_alpha() to ensure that any 
+            # transparency is preserved.
+            if self.sprite_image == None:
+                self.sprite_image = pygame.image.load(image).convert_alpha()
+        else:
+            self.sprite_image = image
         
         # Calculate the number of frames.
         self.frame_count = self.sprite_image.get_width() / frame_width
-        print self.frame_count
         
         # Set up the image size details. Assume that we are using the full
         # height of the image.
         self.frame_width = frame_width
         self.frame_height = self.sprite_image.get_height()
         
-        # Extract the frames into a list
+        # Extract the frames into a list of sub-surfaces
         self.frames = []
         for frame in range(0, self.frame_count):
             offset = frame * self.frame_width
-            frame = self.sprite_image.subsurface(Rect(offset, 0, 64, 64))
+            frame = self.sprite_image.subsurface(Rect(offset, 0, frame_width, frame_width))
             self.frames.append(frame)
             
         # Prepare the first frame.
@@ -120,7 +121,7 @@ class FrameSprite(pygame.sprite.Sprite):
     frames = None
     animation = None
     
-    def __init__(self, imagename, frame_width, speed):
+    def __init__(self, image, frame_width, speed):
         """
         Initialises the sprite, setting up an Animation instance to handle the
         actual animation. See the Animation class for the meaning of the
@@ -129,8 +130,9 @@ class FrameSprite(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         
         # Set up the animation handler
-        self.animation = Animation(imagename, frame_width, speed, self.on_finish)
+        self.animation = Animation(image, frame_width, speed, self.on_finish)
         self.rect = self.animation.rect
+        self.image = self.animation.image
         
         # Set the other parameters
         self.visible = True
@@ -194,7 +196,7 @@ class Asteroids():
     
     def __init__(self, imagename):
         self.roids = pygame.sprite.Group()
-        for i in range(20):
+        for i in range(50):
             self.roids.add(Asteroid(imagename, 64, 10))
 
     def update(self, current_time):
@@ -203,7 +205,89 @@ class Asteroids():
     def draw(self, target):
         rectlist = self.roids.draw(target)
         pygame.display.update(rectlist)
+
+class Pulse(FrameSprite):
+    """
+    Pulse sprites represent weapon-fire (see the WeaponFire class below). For
+    this game weapon-fire is always assumed to travel vertically.
+    """
     
+    def __init__(self, image, on_die):
+        FrameSprite.__init__(self, image, 8, 10)
+
+        # Set the default position, at the front of the ship (the X co-ordinate
+        # representing the ship position will be set via the WeaponFire class).
+        self.rect = Rect(0, 800 - 64, 8, 8)
+
+        # The speed of each pulse is randomised slightly for a better visual
+        # effect (without this the pulses look like a fixed line, because they
+        # always occupy the same vertical positions).
+        self.speed = 12 + random.randint(0, 4)
+        
+        self.next_update_time = 0 # update() hasn't been called yet.
+        
+        self.on_die = on_die
+        
+    def update(self, current_time):
+        FrameSprite.update(self, current_time)
+                
+        if self.next_update_time < current_time:
+
+            # Move us up the screen
+            self.rect.top -= self.speed
+            
+            if self.rect.top < -8:
+                self.on_die(self)
+                
+            self.next_update_time = current_time + 1
+
+class WeaponFire():
+    """
+    Base class for handling weapon-fire, using the Pulse class (above) for the
+    sprites that represent the visible appearance. The WeaponFire class is
+    passed the name of the sprite image to use for the pulses, so this same
+    class can be used for different weapons.
+    
+    REFACTOR: allow this class to use different Pulse classes providing 
+              different behaviours.
+    """
+
+    # Position is the point from which the weapon pulses will emanate. This
+    # must be set externally before the weapon is fired.
+    position = 0
+    firing = False
+    
+    def __init__(self, imagename):
+        # Always start with the weapon inactive
+        self.firing = False
+        
+        # Store the 'pulse' sprites in a sprite group for efficiency
+        self.pulses = pygame.sprite.Group()
+        
+        # Use the same image for all the 'pulse' sprites
+        self.imagename = imagename
+        self.sprite_image = pygame.image.load(self.imagename).convert_alpha()
+            
+    def update(self, current_time):
+        if self.firing:
+            # Create another pulse, place it at the current weapon position, and
+            # add it to the sprite group.
+            pulse = Pulse(self.sprite_image, self.on_pulse_die)
+            pulse.rect.left = self.position
+            self.pulses.add(pulse)
+        # Update any existing pulses            
+        self.pulses.update(current_time)
+    
+    def draw(self, target):
+        # Update the 'pulses' sprite-group (this will redraw all the sprites in
+        # the group)
+        rectlist = self.pulses.draw(target)
+        pygame.display.update(rectlist)
+
+    def on_pulse_die(self, pulse):
+        # The pulse has gone off-screen, so remove it
+        self.pulses.remove(pulse)
+        
 class Ship(pygame.sprite.Sprite):
     """
     Controls and displays the player's ship
@@ -291,6 +375,9 @@ class Game(object):
         self.explosion.visible = False
         self.ship.collided = False
         
+        self.weapon = WeaponFire(os.path.join("graphics", "weapon_01.png"))
+        self.weapon.firing = False
+        
         # Prepare the asteroids
         self.asteroids = Asteroids(os.path.join("graphics", "asteroid_frames_01.png"))
         
@@ -305,22 +392,31 @@ class Game(object):
     # --------------------------------------------------------------------------
 
     def on_keydown(self, key):
-        if key == K_RIGHT:
-            self.ship.apply_thrust(1, 0)
-                
         if key == K_LEFT:
             self.ship.apply_thrust(-1, 0)
             
+        if key == K_RIGHT:
+            self.ship.apply_thrust(1, 0)
+                
         if key == K_UP:
-            self.ship.apply_thrust(0, -1)
+            # TODO: Change weapon
+            pass
             
         if key == K_DOWN:
-            self.ship.apply_thrust(0, 1)
+            # TODO: Change weapon
+            pass
+            
+        if key == K_SPACE:
+            self.weapon.firing = True
         
     # --------------------------------------------------------------------------
 
     def on_keyup(self, key):
-        self.ship.release_thrust(0, 0)        
+        if key == K_RIGHT or key == K_LEFT:
+            self.ship.release_thrust(0, 0)
+            
+        if key == K_SPACE:
+            self.weapon.firing = False        
         
     # --------------------------------------------------------------------------
 
@@ -342,13 +438,26 @@ class Game(object):
         # Update the asteroid positions
         self.asteroids.update(current_time)
         
-        # Check for collisions
+        # Update any weapon fire. Set the position so that the weapon-fire
+        # appears from the middle of the ship
+        self.weapon.position = self.ship.rect.left + 32 - 4
+        self.weapon.update(current_time)
+        
+        # Check for collisions with asteroids
         collision = pygame.sprite.spritecollide(self.ship, self.asteroids.roids, True)
         if collision:
             # Show explosion
             self.ship.collided = True
             self.explosion.rect = collision[0].rect
             self.explosion.visible = True
+
+        # Check for hitting asteroids with weapon-fire
+        for pulse in self.weapon.pulses:
+            collision = pygame.sprite.spritecollide(pulse, self.asteroids.roids, True)
+            if collision:
+                # Show explosion
+                self.explosion.rect = pulse.rect
+                self.explosion.visible = True
 
         if self.ship.collided:
             self.explosion.update(current_time)
@@ -390,16 +499,14 @@ class Game(object):
         # Draw the asteroids
         self.asteroids.draw(self.display)
         
+        # Draw the weapon fire
+        self.weapon.draw(self.display)
+        
         # Update the display
         pygame.display.update()
         
     # --------------------------------------------------------------------------
 
-    def on_quit(self, sender):
-        self.running = False
-
-    # --------------------------------------------------------------------------
-    
     def run(self):
         """
         Main game loop
